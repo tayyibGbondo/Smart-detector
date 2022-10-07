8eb015f391f495c09e27d0775f824ea04be50358
Tayyib 4 Tech <gbondotahib72@gmaila.com>
2022-10-07 11:41:30 +0000

    popup notifacation for the download button

--- js/app.js
+++ js/app.js
@@ -1,10 +1,28 @@
 //DOM ELEMENTS
 const toggleButton = document.querySelector('.toggle-btn'),
 nav = document.querySelector('.nav-links'),
-logo = document.querySelector('.logo');
+logo = document.querySelector('.logo'),
+downloadButton = document.querySelector('.download-btn'),
+popOver = document.querySelector('.popup'),
+okButton = document.querySelector('.okButton');
 
 toggleButton.addEventListener('click', event => {
   nav.classList.toggle('is-active')
   logo.classList.toggle('rem')
 })
 
+
+//the download button
+downloadButton.addEventListener('click', event => {
+  if(popOver.classList.contains('not-live')){
+    popOver.classList.remove('not-live')
+  }
+})
+
+//the ok button for the popup notifacation
+// okButton.addEventListener('click', event => {
+//   popOver.style.display = "none"
+// })
+function removePopUp(){
+  popOver.classList.add('not-live')
+}
